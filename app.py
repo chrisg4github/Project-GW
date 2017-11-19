95e3e95ce1a7b04b3be20a3cf3066d9f4c66087f
chrisg4github <chrisguthrie1600@comcast.net>
2017-11-19 07:59:19 -0800

    Save app.py changes 11/19

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
  
-@app.route('/gw/v1.0')
+@app.route('/')
 def index():
     return render_template('./html/%s.html' % ('landing_page'))
  
